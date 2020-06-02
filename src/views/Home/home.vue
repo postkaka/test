3cd2ff20dcc6786711de19d8dd232c8a5a942384
unknown <you@example.com>
2020-06-02 22:17:26 +0800

    添加首页轮播图

--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -3,22 +3,34 @@
   <nav-bar class="home-nav">
     <div slot="center">购物街</div>
   </nav-bar>
+  <home-swiper :banners="banners"></home-swiper>
+
 </div>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
   import {getHomeMultidata} from "../../network/home";
+  import HomeSwiper from "./childComps/HomeSwiper";
 
   export default {
         name: "home",
       components: {
-          NavBar
+          NavBar,
+       HomeSwiper,
       },
-  create(){
+    data(){
+        return {
+          banners:[],
+          recommends: []
+        }
+    },
+  created(){
     //1.请求多个数据
     getHomeMultidata().then(res =>{
-      console.log(res);
+      //console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
     })
   }
     };
