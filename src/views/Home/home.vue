bc8b9b0499e85b832565ba446cb015b03dfde2d8
unknown <you@example.com>
2020-06-03 22:09:00 +0800

    home完善

--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -4,20 +4,34 @@
     <div slot="center">购物街</div>
   </nav-bar>
   <home-swiper :banners="banners"></home-swiper>
+  <recommend-view :recommends="recommends"></recommend-view>
+  <feature-view></feature-view>
+  <tab-control :titles="['流行','新款','精选']"></tab-control>
 
 </div>
 </template>
 
 <script>
-  import NavBar from "../../components/common/navbar/NavBar";
-  import {getHomeMultidata} from "../../network/home";
   import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+
+  import TabControl from "../../components/content/tabControl/TabControl";
+  import NavBar from "../../components/common/navbar/NavBar";
+
+  import {getHomeMultidata} from "../../network/home";
 
   export default {
         name: "home",
       components: {
-          NavBar,
+
        HomeSwiper,
+        RecommendView,
+        FeatureView,
+
+        TabControl,
+        NavBar,
       },
     data(){
         return {
@@ -38,8 +52,16 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
 .home-nav{
   background-color: var(--color-tint);
   color: white;
+ position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
 }
 </style>
