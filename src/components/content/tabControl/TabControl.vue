ff08d1c42ed839266ebce621a4508466ba35ee3e
unknown <you@example.com>
2020-06-04 21:39:09 +0800

    完善首页

--- src/components/content/tabControl/TabControl.vue
+++ src/components/content/tabControl/TabControl.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="tab-control">
-      <div v-for="(item,index) in titles" :key="index" >
+      <div v-for="(item,index) in titles" :key="item.id"
+           class="tab-control-item" :class="{active: index === currentIndex}"
+      @click="itemClick(index)">
         <span>{{item}}</span>
       </div>
     </div>
@@ -16,10 +18,39 @@
               return []
             }
           }
+      },
+      data(){
+          return{
+            currentIndex: 0
+          }
+      },
+      methods :{
+          itemClick(index){
+            this.currentIndex = index
+          }
       }
     }
 </script>
 
 <style scoped>
+  .tab-control{
+    display: flex;
+    text-align: center;
+    font-size: 14px;
+  }
+.tab-control-item{
+  flex: 1;
+  height: 40px;
+  line-height: 40px;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+  .active {
+    color: var(--color-hight-text);
 
+  }
+  .active span {
+    border-bottom: 3px solid var(--color-tint);
+  }
 </style>
