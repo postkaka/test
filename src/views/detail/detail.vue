e44ef2ea08aa2ff3f100309e3c473f913dc4cb2a
unknown <you@example.com>
2020-06-15 22:12:57 +0800

    详情页

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,19 +1,36 @@
 <template>
 <div class="detail">
-{{iid}}
+<detail-nav-bar></detail-nav-bar>
+  <detail-swiper :top-images="topImages"></detail-swiper>
 </div>
 </template>
 
 <script>
+  import DetailNavBar from "./childComps/DetailNavBar";
+  import {getDetail} from "network/detail"
+  import DetailSwiper from "./childComps/DetailSwiper";
     export default {
         name: "detail",
+      components:{
+          DetailNavBar,
+          DetailSwiper
+      },
       data() {
           return{
-           iid: null
+           iid: null,
+            topImages: []
           }
       },
       created() {
+          //1.保持传入的iid
           this.iid = this.$route.params.iid
+          //2.根据iid请求详情数据
+        getDetail(this.iid).then(res => {
+          console.log(res);
+          //1.获取顶部的图片轮播数据
+          this.topImages = res.result.itemInfo.topImages
+          console.log(this.topImages);
+        })
       }
     }
 </script>
