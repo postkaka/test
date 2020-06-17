37d1f87b9bc13ac11b46a7d6e3af7bec26dfa825
postkaka <mayuyululu@163.com>
2020-06-17 16:00:19 +0800

    完善详情页

--- src/views/detail/childComps/detailBaseInfo.vue
+++ src/views/detail/childComps/detailBaseInfo.vue
@@ -1,10 +1,12 @@
 <template>
-    <div v-if="Object.keys(goods).length !==0" class="detail-base-info" >
+    <div v-if="Object.keys(goods).length !==0" id="detail-base-info" >
       <div class="info-title">{{goods.title}}</div>
       <div class="info-price">
-        <span>{{goods.price}}</span>
-        <span>{{goods.oldPrice}}</span>
-        <span> {{ goods.discount }}</span>
+        <span class="info-price-now">{{goods.price}}</span>
+        <del>
+          <span class="info-price-old">{{goods.oldPrice}}</span>
+        </del>
+        <span class="info-price-discount" :style="{backgroundColor: goods.discountBgColor}"> {{goods.discount }}</span>
       </div>
       <div class="info-other">
        <span>{{ goods.columns[0] }}</span>
@@ -18,7 +20,7 @@
       v-for="(item,index) in goods.sercices.length -1"
       :key="index"
       >
-        <img src="goods.sercices[item - 1].icon" alt="">
+        <img :src="goods.sercices[1].icon" alt="">
         <span>{{goods.sercices[item - 1].name}}</span>
       </span>
       </div>
@@ -39,7 +41,52 @@
 </script>
 
 <style scoped>
-.info-title {
-
+  #detail-base-info{
+  padding: 10px;
+  }
+  .info-title {
+    font-size: 18px;
+    color: #000000;
+    padding-bottom: 7px;
 }
+  .info-price-now {
+    font-size: 18px;
+    color: #ff5777;
+  }
+  .info-price-old {
+    font-size: 5px;
+    color: gray;
+    padding: 5px;
+  }
+  .info-price-discount {
+    color: white;
+    border-radius:0.21rem;
+    padding: 0.128rem;
+    font-size: 0.6rem;
+    position: relative;
+    top: -0.21rem;
+    text-align: center;
+  }
+  .info-other {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid rgba(128, 128, 128, 0.3);
+    font-size: 5px;
+    color: gray;
+    padding-top: 10px;
+    padding-bottom: 5px;
+  }
+  .info-service {
+    display: flex;
+    justify-content: space-between;
+    font-size: 6px;
+    color: black;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    border-bottom: 3px solid rgba(128, 128, 128, 0.3);
+
+  }
+  .info-service img {
+    width: 10px;
+  }
 </style>
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,11 +1,13 @@
 <template>
 <div class="detail">
   <detail-nav-bar class="detail-nav"></detail-nav-bar>
-  <scroll class="content">
+  <scroll class="content" ref="scroll">
     <div>
       <detail-swiper :topImages="topImages"></detail-swiper>
       <detail-base-info :goods="goods"></detail-base-info>
-      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-shop-info :shop="shop" ></detail-shop-info>
+      <detail-info :detailInfo="detailInfo" @imageLoad="imageLoad"></detail-info>
+      <detail-param-info :paramInfo="paramInfo"></detail-param-info>
     </div>
   </scroll>
 </div>
@@ -13,11 +15,13 @@
 
 <script>
   import DetailNavBar from "./childComps/DetailNavBar";
-  import {getDetail,Goods,Shop} from "../../network/detail";
+  import {getDetail,Goods,Shop,GoodsParam} from "../../network/detail";
   import DetailSwiper from "./childComps/DetailSwiper";
   import detailBaseInfo from "./childComps/detailBaseInfo";
   import DetailShopInfo from "./childComps/DetailShopInfo";
   import scroll from "../../components/common/scroll/scroll";
+  import DetailInfo from "./childComps/DetailInfo";
+  import DetailParamInfo from "./childComps/DetailParamInfo";
 
 
   export default {
@@ -27,14 +31,18 @@
           DetailSwiper,
           detailBaseInfo,
           DetailShopInfo,
-          scroll
+          scroll,
+          DetailInfo,
+          DetailParamInfo
       },
       data() {
           return{
            iid: null,
             topImages: [],
             goods: {},
-            shop: {}
+            shop: {},
+            detailInfo: {},
+            paramInfo: {}
           }
       },
       created() {
@@ -52,7 +60,18 @@
               //创建店铺信息
               this.shop = new Shop(data.shopInfo)
               console.log(this.shop);
+              // 保存商品的详情数据
+              this.detailInfo = data.detailInfo
+              console.log(this.detailInfo);
+              // 获取参数数据
+              this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
+              console.log(this.paramInfo);
             })
+      },
+      methods: {
+          imageLoad() {
+            this.$refs.scroll.refresh()
+          }
       }
     }
 </script>
@@ -60,13 +79,15 @@
 <style scoped>
 .detail {
   position: relative;
-  z-index: 9;
+  z-index: 1;
   background-color: #fff;
   height: 100vh;
 
 }
   .content {
-    height: calc(100% - 44px);
+    position: absolute;
+    top: 44px;
+    bottom: 60px;
   }
   .detail-nav {
     position: relative;
